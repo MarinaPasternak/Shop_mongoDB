279dfc1c6682711a153afb83a61473c9ef1c1322
Marina_Pasternak <256pasternakmarina@gmail.com>
2026-02-24 21:11:26 +0200

    add express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const User = require('../models/user');
+const {validationResult } = require('express-validator');
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
@@ -71,6 +72,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
 
   User.findOne({ email: email }).then(userDoc => {
     if (userDoc) {
